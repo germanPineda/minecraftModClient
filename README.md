3c72d36a022d358a92a79a9cba57ca2abec01a58
germanPineda <gapu072796@gmail.com>
2022-03-27 16:45:54 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Repositorio de descarga
+Desde este repositorio se podra descargar y actualizar 
+el modpack amlocity, un modpack creado por y para amigos en un
+server privado
+
+(El nombre no tiene ningún significado)
---
+++ install.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+:: First we create the main directory
+ECHO Creando directorios y archivos necesarios, por favor espere
+mkdir amlocity\mods
+type nul > amlocity\.curseclient
+type nul > amlocity\minecraftinstance.json
+PAUSE
